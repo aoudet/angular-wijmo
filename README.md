2dcba55b048827d704f5f0d3ddecbd39a00dc0a2
aoudet <a.oudet@gmail.com>
2021-09-09 09:13:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9-0-0-wijmo-bulletgraphvslinergraph-zszpdw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-0-0-wijmo-bulletgraphvslinergraph-zszpdw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as wijmo from '@grapecity/wijmo';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() {}
+
+}
